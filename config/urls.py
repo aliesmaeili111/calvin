4fd55206310f9303a26224ed9bbd988ca1852de7
Ali <88323690+aliesmaeili111@users.noreply.github.com>
2023-05-20 11:56:56 +0330

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -39,8 +39,8 @@ urlpatterns = [
 
 
 # show picture in admin
-from django.conf import settings
-from django.conf.urls.static import static
-urlpatterns +=  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#from django.conf import settings
+#from django.conf.urls.static import static
+#urlpatterns +=  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = "blog.views.page_not_found_view"
